38a02a53cc3a0193d9c694a8a95e357aaa3b853d
SavvyDev06 <developer.savvasmica@gmail.com>
2021-11-07 23:34:38 +0200

    Changed text and overall small UI improvements

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -2,34 +2,51 @@
   <c-stack :spacing="5" width="20%">
     <c-box :p="2" shadow="md" border-width="1px">
       <c-flex justify="center">
-        <c-button width="80%" margin="5px" border-width="1.6px">AI</c-button>
+        <c-button width="80%" margin="5px" border-width="1.6px"
+          ><c-icon icon="user-secret" /><c-icon
+            name="faEnvelope"
+            size="24px"
+            margin-right="2px"
+          />Overview</c-button
+        >
       </c-flex>
       <c-flex justify="center">
-        <c-button width="80%" margin="5px" border-width="1.6px">AI</c-button>
+        <c-button width="80%" margin="5px" border-width="1.6px"
+          >Configuration</c-button
+        >
       </c-flex>
       <c-flex justify="center">
-        <c-button width="80%" margin="5px" border-width="1.6px">AI</c-button>
+        <c-button width="80%" margin="5px" border-width="1.6px"
+          >Settings</c-button
+        >
       </c-flex>
       <c-flex justify="center">
-        <c-button width="80%" margin="5px" border-width="1.6px">AI</c-button>
+        <c-button width="80%" margin="5px" border-width="1.6px"
+          >Commands</c-button
+        >
       </c-flex>
       <c-flex justify="center">
-        <c-button width="80%" margin="5px" border-width="1.6px">AI</c-button>
+        <c-button width="80%" margin="5px" border-width="1.6px"
+          >Action Logs</c-button
+        >
       </c-flex>
       <c-flex justify="center">
-        <c-button width="80%" margin="5px" border-width="1.6px">AI</c-button>
+        <c-button width="80%" margin="5px" border-width="1.6px"
+          >DeGore Plus</c-button
+        >
       </c-flex>
     </c-box>
   </c-stack>
 </template>
 
 <script>
-import { CButton, CStack } from '@chakra-ui/vue'
+import { CButton, CStack, CIcon } from '@chakra-ui/vue'
 
 export default {
   components: {
     CButton,
     CStack,
+    CIcon,
   },
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,6 @@
+// Import FontAwesome icons
+import { faGlobeAfrica, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -6,20 +9,16 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -40,7 +39,21 @@ export default {
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
+    // NuxtJS emoticons
+    '@nuxtjs/emotion',
   ],
+  chakra: {
+    icons: {
+      // Here we state that we use `fa`
+      // icons library for Chakra's
+      // internal icon parser
+      iconPack: 'fa',
+      iconSet: {
+        faGlobeAfrica,
+        faEnvelope,
+      },
+    },
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
@@ -48,11 +61,10 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
-    }
+      lang: 'en',
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
